e7ec8e8fec4dbe570b556d18ba165d4d7f3651a0
Slawomir Andrzejczak <slawomir_andrzejczak@wp.pl>
2022-10-31 12:18:23 +0100

    Dodanie:     - bindingów do projektu Modyfikacja:     - Activity_main zostało uzupełnione o layout i cały potrzebny kod (w razie czego zakomentowany)

--- app/src/main/java/com/example/foodapp/MainActivity.java
+++ app/src/main/java/com/example/foodapp/MainActivity.java
@@ -1,14 +1,103 @@
 package com.example.foodapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.example.foodapp.R;
+import com.example.foodapp.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Objects;
+
+import javax.annotation.Nullable;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
+
+    private FirebaseFirestore db =  FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = db.collection("Users");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        Objects.requireNonNull(getSupportActionBar()).setElevation(0);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                currentUser = firebaseAuth.getCurrentUser();
+                if(currentUser != null){
+                    currentUser = firebaseAuth.getCurrentUser();
+                    final String currentUserId = currentUser.getUid();
+
+//                    collectionReference.whereEqualTo("userId",currentUserId)
+//                            .addSnapshotListener(new EventListener<QuerySnapshot>() {
+//                                @Override
+//                                public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+//                                    if(error != null){
+//                                        return;
+//                                    }
+//                                    String name;
+//
+//                                    if(!value.isEmpty()){
+//                                        for (QueryDocumentSnapshot snapshot : value){
+//                                            JournalApi journalApi = JournalApi.getInstance();
+//                                            journalApi.setUserId(snapshot.getString("userId"));
+//                                            journalApi.setUsername(snapshot.getString("username"));
+//
+//                                            startActivity(new Intent(MainActivity.this, JournalListActivity.class));
+//                                            finish();
+//                                        }
+//                                    }
+//                                }
+//                            });
+
+
+                }
+            }
+        };
+
+
+
+        //Get started button
+        binding.btnGetStarted.setOnClickListener(view -> {
+//            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        });
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        currentUser = firebaseAuth.getCurrentUser();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (firebaseAuth != null) {
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
     }
 }
