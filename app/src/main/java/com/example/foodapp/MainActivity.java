8fb2079fb7aa53195c0778d54ea73a21e6f01a29
Slawomir Andrzejczak <slawomir_andrzejczak@wp.pl>
2022-11-03 14:37:50 +0100

    v0.3 Dodano:     - Poprawione orderingActivity pod względem graficznym.     - Dodano kilka ikonek.

--- app/src/main/java/com/example/foodapp/MainActivity.java
+++ app/src/main/java/com/example/foodapp/MainActivity.java
@@ -61,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
                                     foodApi.setUserId(snapshot.getString("userId"));
                                     foodApi.setUsername(snapshot.getString("username"));
 
-                                    startActivity(new Intent(MainActivity.this, FoodListActivity.class));
+                                    startActivity(new Intent(MainActivity.this, OrderingActivity.class));
                                     finish();
                                 }
                             }
